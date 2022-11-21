8d9414f63bf51a5b0a9365d60925130a6dcdcaac
dan5 <dngzz1@gmail.com>
2022-11-21 06:41:55 +0000

    added tests, fixed bugs

--- src/main.rs
+++ src/main.rs
@@ -50,30 +50,47 @@ fn create_stack_count(breakpoints: &[(f32, Cap)]) -> Vec<usize> {
     count
 }
 
+fn slice_index(
+    meeting: &(f32, f32),
+    breakpoints: &[(f32, Cap)],
+    num_meetings: usize,
+) -> (usize, usize) {
+    let meeting_start = meeting.0;
+    let meeting_end = meeting.1;
+    let mut start_index = 0;
+    while breakpoints[start_index].0 < meeting_start
+        || (breakpoints[start_index].0 == meeting_start && breakpoints[start_index].1 == Cap::End)
+    {
+        start_index += 1;
+    }
+    let mut end_index = start_index;
+    while end_index < 2 * num_meetings && breakpoints[end_index].0 < meeting_end {
+        end_index += 1;
+    }
+    (start_index, end_index + 1)
+}
+
 // This function has O(n^2): O(n) for looping through meetings and O(n) for finding max_overlap of each meeting.
 fn solve_max_overlap(meetings: &[(f32, f32)]) -> Vec<usize> {
+    is_valid(meetings);
     let breakpoints = create_breakpoints(&meetings);
     let stack_count = create_stack_count(&breakpoints);
     let mut result = vec![];
     for meeting in meetings {
-        let meeting_start = meeting.0;
-        let meeting_end = meeting.1;
-        let mut start_index = 0;
-        while breakpoints[start_index].0 < meeting_start
-            || (breakpoints[start_index].0 == meeting_start
-                && breakpoints[start_index].1 == Cap::End)
-        {
-            start_index += 1;
-        }
-        let mut index = start_index;
-        let mut max_overlap = stack_count[index];
-        while breakpoints[index].0 <= meeting_end && index < meetings.len() {
+        let (start_index, end_index) = slice_index(&meeting, &breakpoints, meetings.len());
+        let mut max_overlap = stack_count[start_index];
+        for index in start_index..end_index {
             max_overlap = std::cmp::max(stack_count[index], max_overlap);
-            index += 1;
         }
         result.push(max_overlap);
     }
     result
+}
+
+fn is_valid(meetings: &[(f32, f32)]) {
+    for meeting in meetings {
+        assert!(meeting.0 < meeting.1);
+    }
 }
 
 #[cfg(test)]
@@ -130,14 +147,47 @@ mod tests {
     }
 
     #[test]
-    fn test_max_overlap_consecutive() {
+    fn test_1313() {
+        let meetings = vec![(1., 3.), (1., 3.)];
+        let max_overlap = solve_max_overlap(&meetings);
+        assert_eq!(max_overlap, vec![2, 2]);
+    }
+
+    #[test]
+    fn test_1335() {
         let meetings = vec![(1., 3.), (3., 5.)];
         let max_overlap = solve_max_overlap(&meetings);
         assert_eq!(max_overlap, vec![1, 1]);
     }
 
     #[test]
-    fn test_max_overlap_consecutive_reversed() {
+    fn test_123536() {
+        let meetings = vec![(1., 2.), (3., 5.), (3., 6.)];
+        let max_overlap = solve_max_overlap(&meetings);
+        assert_eq!(max_overlap, vec![1, 2, 2]);
+    }
+
+    #[test]
+    fn test_133536() {
+        let meetings = vec![(1., 3.), (3., 5.), (3., 6.)];
+        let breakpoints = create_breakpoints(&meetings);
+        assert_eq!(
+            breakpoints,
+            vec![
+                (1., Cap::Start),
+                (3., Cap::End),
+                (3., Cap::Start),
+                (3., Cap::Start),
+                (5., Cap::End),
+                (6., Cap::End)
+            ]
+        );
+        let max_overlap = solve_max_overlap(&meetings);
+        assert_eq!(max_overlap, vec![1, 2, 2]);
+    }
+
+    #[test]
+    fn test_3513() {
         let meetings = vec![(3., 5.), (1., 3.)];
         let breakpoints = create_breakpoints(&meetings);
         assert_eq!(
@@ -154,39 +204,37 @@ mod tests {
     }
 
     #[test]
-    fn test_max_overlap_nested() {
+    fn test_1534() {
         let meetings = vec![(1., 5.), (3., 4.)];
         let max_overlap = solve_max_overlap(&meetings);
         assert_eq!(max_overlap, vec![2, 2]);
     }
 
     #[test]
-    fn test_max_overlap_staggered() {
+    fn test_1325() {
         let meetings = vec![(1., 3.), (2., 5.)];
         let max_overlap = solve_max_overlap(&meetings);
         assert_eq!(max_overlap, vec![2, 2]);
     }
 
     #[test]
-    fn test_max_overlap_three_two() {
-        let meetings = vec![(1., 10.), (1., 2.), (1., 3.), (8., 9.)];
-        let breakpoints = create_breakpoints(&meetings);
-        assert_eq!(
-            breakpoints,
-            vec![
-                (1., Cap::Start),
-                (1., Cap::Start),
-                (1., Cap::Start),
-                (2., Cap::End),
-                (3., Cap::End),
-                (8., Cap::Start),
-                (9., Cap::End),
-                (10., Cap::End)
-            ]
-        );
-        let stack_count = create_stack_count(&breakpoints);
-        assert_eq!(stack_count, vec![1, 2, 3, 2, 1, 2, 1, 0]);
+    fn test_19121367() {
+        let meetings = vec![(1., 9.), (1., 2.), (1., 3.), (6., 7.)];
         let max_overlap = solve_max_overlap(&meetings);
         assert_eq!(max_overlap, vec![3, 3, 3, 2]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_end_time_before_start_time() {
+        let meetings = vec![(3., 1.), (5., 6.)];
+        solve_max_overlap(&meetings);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_end_time_equal_start_time() {
+        let meetings = vec![(1., 1.)];
+        solve_max_overlap(&meetings);
     }
 }
